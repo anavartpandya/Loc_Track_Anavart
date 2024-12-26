e67c87b61d03080fc2720f744cf4c6ca84fa7a54
Anavart Pandya <anavart.pandya@Anavarts-MacBook-Air.local>
2024-12-26 12:25:05 +0530

    Modified server.py file to update the app.run code

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -21,4 +22,5 @@ def update_location():
         return jsonify({"status": "error", "message": "Invalid data"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(host='0.0.0.0', port=port)
