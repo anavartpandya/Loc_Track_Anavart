bb943308058f5dffbeb50475fb3d4aea242e6244
Anavart Pandya <anavart.pandya@Anavarts-MacBook-Air.local>
2025-01-01 19:52:52 +0530

    Added time recording in server.py and sending that value to the Database

--- server.py
+++ server.py
@@ -293,4 +293,5 @@ def show_map():
 
 if __name__ == '__main__':
     eventlet.monkey_patch()
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    # socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5050, debug=False)
